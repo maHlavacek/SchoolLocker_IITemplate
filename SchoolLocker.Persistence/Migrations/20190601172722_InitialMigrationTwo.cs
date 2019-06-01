86825adb06dc16106c32e687764f521b361283a7
maHlavacek <martin_hlavacek@icloud.com>
2019-06-01 20:52:32 +0200

    Validations disabled to add data to DB

---
+++ SchoolLocker.Persistence/Migrations/20190601172722_InitialMigrationTwo.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolLocker.Persistence.Migrations
+{
+    public partial class InitialMigrationTwo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "Pupils",
+                newName: "Version");
+
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "Lockers",
+                newName: "Version");
+
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "Bookings",
+                newName: "Version");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Version",
+                table: "Pupils",
+                newName: "Timestamp");
+
+            migrationBuilder.RenameColumn(
+                name: "Version",
+                table: "Lockers",
+                newName: "Timestamp");
+
+            migrationBuilder.RenameColumn(
+                name: "Version",
+                table: "Bookings",
+                newName: "Timestamp");
+        }
+    }
+}
--- SchoolLocker.Persistence/UnitOfWork.cs
+++ SchoolLocker.Persistence/UnitOfWork.cs
@@ -37,14 +37,14 @@ namespace SchoolLocker.Persistence
         /// </summary>
         public int SaveChanges()
         {
-            var entities = _dbContext.ChangeTracker.Entries()
-                .Where(entity => entity.State == EntityState.Added
-                                 || entity.State == EntityState.Modified)
-                .Select(e => e.Entity);
-            foreach (var entity in entities)
-            {
-                ValidateEntity(entity);
-            }
+            //var entities = _dbContext.ChangeTracker.Entries()
+            //    .Where(entity => entity.State == EntityState.Added
+            //                     || entity.State == EntityState.Modified)
+            //    .Select(e => e.Entity);
+            //foreach (var entity in entities)
+            //{
+            //    ValidateEntity(entity);
+            //}
 
             return _dbContext.SaveChanges();
         }
