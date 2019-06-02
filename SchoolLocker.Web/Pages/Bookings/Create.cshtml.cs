2321b1595c5f0bf85c7f7939a1ddc45ad92cb017
maHlavacek <martin_hlavacek@icloud.com>
2019-06-02 10:07:37 +0200

    BookingDTO filled

--- SchoolLocker.Web/Pages/Bookings/Create.cshtml.cs
+++ SchoolLocker.Web/Pages/Bookings/Create.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SchoolLocker.Core.Contracts;
 using SchoolLocker.Core.Entities;
+using SchoolLocker.Web.DataTransferObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,7 +17,7 @@ namespace SchoolLocker.Web.Pages.Bookings
         public int LockerNumber { get; set; }
 
         [BindProperty]
-        public Booking Booking { get; set; }
+        public BookingDTO BookingDTO { get; set; }
 
         public List<SelectListItem> Pupils { get; set; }
 
@@ -34,7 +36,23 @@ namespace SchoolLocker.Web.Pages.Bookings
                         $"{p.FirstName} {p.LastName}"
                         , p.Id.ToString()))
                         .ToList();
+            BookingDTO = new BookingDTO
+            {
+                LockerNumber = lockerNumber,
+                From = DateTime.Now.Date,
+                PupilId = 0
+            };
 
+        }
+        public IActionResult OnPostCreate()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+          //  _unitOfWork.BookingRepository.Add(BookingDTO);
+           // _unitOfWork.SaveChanges();
+            return RedirectToPage("/Index");
         }
     }
 }
